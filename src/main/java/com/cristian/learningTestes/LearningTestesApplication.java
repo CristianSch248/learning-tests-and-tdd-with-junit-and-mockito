ab85e5fcd3457493fe6756ce0dbd22a5f248e4b7
cristian Schmitzhaus <cristianschmitzhaus2000@gmail.com>
2025-12-15 19:48:41 -0300

    Initial commit

---
+++ src/main/java/com/cristian/learningTestes/LearningTestesApplication.java
@@ -0,0 +1,13 @@
+package com.cristian.learningTestes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningTestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningTestesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cristian/learningTestes/LearningTestesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristian.learningTestes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningTestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
